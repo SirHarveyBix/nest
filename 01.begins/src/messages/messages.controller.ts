cf70d87813229938567282aab97a4fad52214061
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-10 00:17:38 +0100

    error management.

--- 01.begins/src/messages/messages.controller.ts
+++ 01.begins/src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -20,7 +27,12 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messageService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messageService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('This message dosent exist.');
+    }
+    return message;
   }
 }
