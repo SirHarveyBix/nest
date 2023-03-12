24e2d5ef581d79856bc3f29d05f9dcc942015d68
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-12 17:25:23 +0100

    init database connection + creation de user et report entity

--- 03.big-project/src/app.module.ts
+++ 03.big-project/src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { ReportsModule } from './reports/reports.module';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/user.entity';
+import { ReportEntity } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [UserEntity, ReportEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
