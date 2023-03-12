2ab4151be164b738a02d3446d571f7ce42122e0f
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-13 00:26:02 +0100

    creation de la route create  user

--- 03.big-project/src/users/users.controller.ts
+++ 03.big-project/src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dt';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    return body;
+    this.userService.create(body.email, body.password);
   }
 }
