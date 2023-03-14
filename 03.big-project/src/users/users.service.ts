10b39571a63e5c649a69d178cd7a94d996b688ea
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-15 00:03:14 +0100

    creation du repo (users.service) et du DTO

--- 03.big-project/src/users/users.service.ts
+++ 03.big-project/src/users/users.service.ts
@@ -14,5 +14,33 @@ export class UsersService {
 
     this.repo.save(user);
     // this.repo.save({ email, password }); => does not execute Hook (like: @AfterInsert())
+    // it needs to recieve a plain object
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<UserEntity>) {
+    const userToUpdate = await this.repo.findOneBy({ id });
+    if (!userToUpdate) {
+      throw new Error('user not found');
+    }
+
+    Object.assign(userToUpdate, attrs);
+    return this.repo.save(userToUpdate);
+  }
+
+  async remove(id: number) {
+    const userToRemove = await this.repo.findOneBy({ id });
+    if (!userToRemove) {
+      throw new Error('user not found');
+    }
+
+    return this.repo.remove(userToRemove);
   }
 }
