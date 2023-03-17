4a04bf3b736bb017a9da75fcadd5597e3a7258e8
SirHarveyBix <gui.lefort.17@gmail.com>
2023-03-17 18:17:41 +0100

    gestion des donnée a exculre lors de la recuperation des données

--- 03.big-project/src/users/user.entity.ts
+++ 03.big-project/src/users/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import {
   AfterInsert,
   AfterRemove,
@@ -11,9 +12,12 @@ import {
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
+  @Exclude()
   password: string;
 
   @AfterInsert()
