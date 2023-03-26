1fe119acfc8013cc9eadc197e2e7b82711d10318
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-26 15:03:12 +0200

    better typing + improve script for tests

--- 03.big-project/src/users/auth.service.spec.ts
+++ 03.big-project/src/users/auth.service.spec.ts
@@ -1,20 +1,24 @@
 import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
+import { UserEntity } from './user.entity';
 
 it('can create instance of auth service', async () => {
-  const fakeUserSerivce = {
+  const fakeUserSerivce: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as UserEntity),
   };
   const module = await Test.createTestingModule({
     providers: [
       AuthService,
-      { provide: UsersService, useValue: fakeUserSerivce },
+      {
+        provide: UsersService,
+        useValue: fakeUserSerivce,
+      },
     ],
   }).compile();
-  const service = module.get(AuthService);
 
+  const service = module.get(AuthService);
   expect(service).toBeDefined();
 });
