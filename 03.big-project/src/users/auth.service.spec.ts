c349c898444d995b73c0ab2136441a760c2da5e1
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-26 15:23:48 +0200

    add create user + throw error tests

--- 03.big-project/src/users/auth.service.spec.ts
+++ 03.big-project/src/users/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { UserEntity } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUserSerivce: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUserSerivce: Partial<UsersService> = {
+    fakeUserSerivce = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as UserEntity),
@@ -27,5 +29,23 @@ describe('AuthService', () => {
 
   it('can create instance of auth service', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('create a new user with salted & hashed password', async () => {
+    const user = await service.signup('test@test', 'pass');
+    expect(user.password).not.toEqual('pass');
+
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if signing up with already existing email', async () => {
+    fakeUserSerivce.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as UserEntity]);
+
+    await expect(service.signup('test@test', 'pass')).rejects.toThrow(
+      BadRequestException,
+    );
   });
 });
