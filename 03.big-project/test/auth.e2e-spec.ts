cd9406ec4dc250599c9b65f420487ee2e68ef129
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-05-25 13:24:53 +0200

    refactor signup to signup function

--- 03.big-project/test/auth.e2e-spec.ts
+++ 03.big-project/test/auth.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('Authentication', () => {
   let app: INestApplication;
+  const createdEmail = 'test@test.test';
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,33 +16,30 @@ describe('Authentication', () => {
     await app.init();
   });
 
-  it('POST - signup : /auth/signup', async () => {
-    const createdEmail = 'test@test.test';
-
+  const signup = async (email = 'test@test.test') => {
     return request(app.getHttpServer())
-      .post('/auth/signup')
-      .send({ email: createdEmail, password: 'test' })
-      .expect(201)
-      .then((response) => {
-        const { id, email } = response.body;
-        expect(id).toBeDefined();
-        expect(email).toEqual(email);
-      });
-  });
-
-  it('GET - current user : /auth/whoami', async () => {
-    const email = 'test@two.test';
-    const res = await request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'pass' })
       .expect(201);
-    const cookie = res.get('Set-Cookie');
+  };
+
+  it('POST - signup : /auth/signup', async () => {
+    return signup().then((response) => {
+      const { id, email } = response.body;
+      expect(id).toBeDefined();
+      expect(email).toEqual(createdEmail);
+    });
+  });
+
+  it('GET - current user : /auth/whoami', async () => {
+    const response = await signup();
+    const cookie = response.get('Set-Cookie');
 
     const { body } = await request(app.getHttpServer())
       .get('/auth/whoami')
       .set('Cookie', cookie)
       .expect(200);
 
-    expect(body.email).toEqual(email);
+    expect(body.email).toEqual(createdEmail);
   });
 });
