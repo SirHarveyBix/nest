86ee0b660fb3be74aad1909bbe3ba4d212695c45
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-05-25 12:58:09 +0200

    test des routes : /auth/signup & /auth/whoami

--- 03.big-project/test/auth.e2e-spec.ts
+++ 03.big-project/test/auth.e2e-spec.ts
@@ -15,19 +15,33 @@ describe('Authentication', () => {
     await app.init();
   });
 
-  // afterEach(async () => {});
-
   it('POST - signup : /auth/signup', async () => {
-    const email = 'tekjst@test.test';
+    const createdEmail = 'test@test.test';
 
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email, password: 'test' })
+      .send({ email: createdEmail, password: 'test' })
       .expect(201)
       .then((response) => {
         const { id, email } = response.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('GET - current user : /auth/whoami', async () => {
+    const email = 'test@two.test';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'pass' })
+      .expect(201);
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
